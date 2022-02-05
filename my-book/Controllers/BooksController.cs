df5eab3c4dbb4a7877c0c92ece5ffb7c8d55c5e8
kraaz2077 <krajeevk@otlook.com>
2022-02-04 23:56:24 -0600

    Post Method

---
+++ my-book/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_book.Data.Services;
+using my_book.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_book.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksService _booksService;
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVM book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+    }
+}
---
+++ my-book/Data/Services/BooksService.cs
@@ -0,0 +1,37 @@
+﻿using my_book.Data.Models;
+using my_book.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_book.Data.Services
+{
+    public class BooksService
+    {
+        public AppDbContext _context;
+
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM book) 
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value :null,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+    }
+}
